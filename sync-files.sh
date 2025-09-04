0c0213312b2baf3af9c2285598da51f0c2dd3774
Artjom Kurapov <artkurapov@gmail.com>
2025-09-04 20:09:08 +0300

    ...

--- sync-files.sh
+++ sync-files.sh
@@ -1,4 +1,4 @@
 while true; do
-  rsync -avz --update gratheon@jetson-orin:/home/gratheon/git/entrance-observer/videos ~/git/entrance-observer/remote-videos/
+  rsync -avz --update --partial --inplace gratheon@jetson-orin:/home/gratheon/git/entrance-observer/videos ~/git/entrance-observer/remote-videos/
   sleep 10
 done
